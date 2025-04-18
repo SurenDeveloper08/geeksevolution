03dd6493acac649706d5a2f98f32eada26d11469
SurenDeveloper08 <surendirandevloper@gmail.com>
2025-04-18 15:11:49 +0400

    imgtest

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
         <Header />
         <Routes>
          <Route path="/" element={<Home />} />
+         <Route path="" element={<Home />} />
          <Route  path='/about' element={<About/>} />
           <Route  path='/blog' element={<Blog/>} />
           <Route  path='/service' element={<Service/>} />
